14ae9363bb68b31b5534ac29550db16196d137ee
edisson89 <edisson.12@hotmail.com>
2023-03-22 10:38:23 -0500

    AGregando actualizaciones

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import styles from  '../App.module.css'
+
+export default function Card(props) {
+   const {id,name, species,gender,image,onClose}= props
+   return (
+      <div className={styles.card}>
+         <button id="cardButton" onClick={()=>onClose(id)}>X</button>
+         <h2>{name}</h2>
+         <h2>{species}</h2>
+         <h2>{gender}</h2>
+         <img className={styles.image} src={image} alt="imagen"/> 
+      </div>
+   );
+}
