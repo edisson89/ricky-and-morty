3ef6ce833a15f4124f6cd7f24cc6ca2841bd1da3
edisson89 <edisson.12@hotmail.com>
2023-03-22 10:41:43 -0500

    Agregando axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./App.module.css";
+import Cards from "./components/Cards.jsx";
+import Nav from "./components/Nav";
+import axios from "axios";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+ async function onSearch (id){
+  try{
+     const data = await axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+      
+        
+        let existe = characters.filter((ch)=> ch.id === id)
+        console.log(existe)
+        if(existe){
+          alert('Ya Existe')
+        }else{
+          setCharacters((oldChars) => [...oldChars, data]);
+        }
+        
+      
+  }catch (error) {
+        // handle error
+        console.log(error);
+      };
+    }
+
+  const onClose = (id) => {
+    setCharacters((oldChars) => {
+      return oldChars.filter((item) => item.id !== id);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className={styles.App}>
+      <Routes>
+        {/* <Route path="/" element={Home}></Route>
+        <Route path="/" element={}></Route>
+        <Route path="/" element={Home}></Route> */}
+
+          <Nav onSearch={onSearch} />
+         <Cards onClose={onClose} characters={characters} />
+
+      <hr />
+      </Routes>
+    
+    
     </div>
   );
 }
