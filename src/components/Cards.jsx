8ce180db22de2b95a1775f758ac45be82d3aa099
edisson89 <edisson.12@hotmail.com>
2023-03-22 10:42:48 -0500

    destructuring props

---
+++ src/components/Cards.jsx
@@ -0,0 +1,22 @@
+import Card from "./Card";
+import styles from "../App.module.css";
+
+export default function Cards(props) {
+  const { characters, onClose } = props;
+ 
+  return (
+    <div className={styles.cards}>
+      {characters &&
+       characters.map(({ name, species, gender, image, id }) => (
+        <Card
+          key={id}
+          name={name}
+          species={species}
+          gender={gender}
+          image={image}
+          onClose={onClose}
+        />
+      ))}
+    </div>
+  );
+}
