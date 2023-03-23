733726c889ac70d97e52a1feb86681d8d8e03c9a
edisson89 <edisson.12@hotmail.com>
2023-03-22 21:41:24 -0500

    Agregando BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,8 +7,10 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <BrowserRouter>
+<StrictMode>
   <App />
-</BrowserRouter>
+</StrictMode>
+  </BrowserRouter>
   
   
 );
