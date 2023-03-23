019f6d55cee5030a73499b9941683d9f22a25a16
edisson89 <edisson.12@hotmail.com>
2023-03-22 21:40:57 -0500

    Agregando Links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import styles from '../App.module.css'
+import { Link } from 'react-router-dom'
+
 
 const Nav = ({onSearch}) => {
   return (
     <div className={styles.navbar}>
       
+      <Link to='home'>
+      <button>Home</button></Link>
+
+      <Link to='about'>
+      <button >About</button>
+      </Link>
         <div className={styles.searchBar}>
           <SearchBar onSearch={onSearch}/>
         </div>
