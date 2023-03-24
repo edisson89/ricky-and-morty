01ef63b2595ecf93a49f068d86034e139740154a
edisson89 <edisson.12@hotmail.com>
2023-03-23 22:35:42 -0500

    about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1>Sobre de nosotros</h1>
+      <p>
+      Plantilla de la página 
+Hay cuatro componentes principales en una página Acerca de nosotros:
+
+1. Compartir la historia de por qué se fundó la empresa
+
+Esta es tu oportunidad de centrarte en el «por qué» de tu empresa Podría ser lo que te diferencia de otros competidores en el mercado.
+
+Comparte el momento en que obtuviste la inspiración para tu empresa y lo que te motivó a ponerla en marcha.
+
+2. Destaca tu trayectoria y el papel de tu equipo fundador
+
+¿Quién eres? ¿Por qué eres la persona adecuada para dirigir tu empresa en este momento? Comparte tu historial y tu historia personal. Eso te conectará con tus clientes.
+
+3. Documenta la evolución de la empresa
+
+Expón todo, desde los obstáculos hasta las mejoras del producto. Pon al día a tus nuevos clientes. ¿Dónde está la empresa hoy? ¿En qué se diferencia de donde estaba antes?
+
+4. Documenta la misión y la visión
+
+¿Qué intenta resolver tu empresa? ¿Hacia dónde se dirige? Termina la página sobre nosotros detallando los pasos que estás dando para transformar la empresa en tu visión final.
+
+Con estos cuatro componentes, crearás una página estelar sobre nosotros que asombrará a los clientes potenciales y convertirá a nuevos clientes.
+
+Combinar estos elementos con el constructor de sitios web adecuado y un diseño de página apropiado te ayudará a transformar a un visitante en un cliente.
+
+También es importante recordar lo que quieren tus clientes.
+
+Los usuarios de tu público objetivo quieren ver la declaración de tu misión, la prueba social y un ejemplo de uso de tu producto. Estos elementos en tu página web Sobre nosotros generarán confianza con el público objetivo.
+
+Ahora, veamos algunas empresas que han defendido esos componentes y les han dado vida.
+
+
+      </p>
+    </div>
   )
 }
 
-export default About
+export default About;
