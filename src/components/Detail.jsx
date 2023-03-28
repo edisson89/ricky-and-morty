fb22836a4c050b5e0a266d668468414bbbb579fe
edisson89 <edisson.12@hotmail.com>
2023-03-28 14:48:28 -0500

    Agregando detalles

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,33 +1,34 @@
- import axios from 'axios'
- import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styles from '../App.module.css'
+import styles from "../App.module.css";
 
 const Detail = () => {
-  const [character, setCharacter] = useState({})
+  const [character, setCharacter] = useState({});
   const { id } = useParams();
-     useEffect(() => {
-      
-        axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-        .then(({ data }) => {
-            if (data.name) {
-               setCharacter(data);
-             } else {
-                window.alert("No hay personajes con  ese id");
-              }
-            });
-            return setCharacter({})
-     }, [id])
-     
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(({ data }) => {
+        if (data.name) {
+          setCharacter(data);
+        } else {
+          window.alert("No hay personajes con  ese id");
+        }
+      });
+    return setCharacter({});
+  }, [id]);
 
   return (
-  <div>
-    <h2>{character.name}</h2>
-         <h2>{character.species}</h2>
-         <h2>{character.gender}</h2>
-         <img className={styles.image} src={character.image} alt="imagen"/> 
-  </div>
-  )
+    <div>
+      <h2>{character.name}</h2>
+      <h2>{character.species}</h2>
+      <h2>{character.status}</h2>
+      <h2>{character.gender}</h2>
+      <h2>{character.origin?.name}</h2>
+      <img className={styles.image} src={character.image} alt="imagen" />
+    </div>
+  );
 };
 
 export default Detail;
