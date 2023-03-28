912b058c0b2869300057e8e559aade9f633160c0
edisson89 <edisson.12@hotmail.com>
2023-03-28 14:48:41 -0500

    Vaalidaando Email

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,38 +1,55 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "../App.module.css";
 
-const Login = () => {
+const Login = ({login}) => {
   const [input, setInput] = useState({
     email: "",
     password: "",
   });
+
   const [error, setError] = useState({
     email: "",
     password: "",
   });
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
- const regexEmail = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
-const regextPassword = "^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
-  
-function handleChange(params) {}
+  const regexEmail =
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  const regexPassword = / ^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$ /;
 
-  function validate(inputs) {
-    if (!inputs.email) {
+
+  function validate(input) {
+    const error = {};
+
+    if (!input.email) {
       error.email = "Debe haber un Email";
-    } else if (!inputs.password) {
-      error.email = "Debe haber un Email";
-    }
-     else if (!regexEmail.test(input.email)) {
-      error.email = "error";
-     }
-     else if (!regextPassword.test(input.password)) {
-      error.email = "error";
-     }else{
-      navigate('/')
-     }
-     
+    } else if (!input.password) {
+      error.password = "Debe haber un Password";
+    } 
+    //else if (!regexEmail.test(input.email)) {
+    //   error.email = "Debe haber un Email valido";
+    // } else if (!regexPassword.test(input.password)) {
+    //   error.password = "Debe haber un Password valido";
+    // }
+    return error;
   }
+
+  function handleChange(event) {
+      event.preventDefault()
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+    setError(
+      validate({
+        ...error,
+        [event.target.name]: event.target.value,
+      })
+    );
+  }
+
+  
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -46,20 +63,41 @@ function handleChange(params) {}
         email: "",
         password: "",
       });
+      login(input);
+      return alert("OK");
     }
+    navigate("/");
+    return alert("Error");
   }
   return (
     <div>
-      <form onSubmit={handleSubmit} action="">
-        <label htmlFor="email">Email:</label>
+      <form onSubmit={handleSubmit}>
+        <label name="email">Email: </label>
+          <input
+          type="email"
+            value={input.email}
+            name="email"
+            onChange={handleChange}
+            placeholder="Ingrese su Email"
+          />
+       { error.email &&  <p className={styles.error}>{error.email}</p> }
+        
+        <label name="pasword">Password</label>
         <input
-          name="email"
-          type="text"
-          onchange={handleChange}
-          value={input.email}
+          type='password'
+          name="password"
+          onChange={handleChange}
+          value={input.password}
+          placeholder="Ingrese su Contraseña"
         />
-        <label htmlFor="paswoord">Password</label>
-        <input name="password" type="text" />
+  {error.password &&  <p className={styles.error}>{error.password}</p>}
+     
+
+        {Object.keys(error).length === 0 ? 
+      (  <Link to="/home">
+        <button type="submit">Enviar</button>
+        </Link> ): <h2>Revise sus datos</h2>
+        }
       </form>
     </div>
   );
