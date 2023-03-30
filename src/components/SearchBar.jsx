568385b2e5c8c5843fa8837ded31f8435dba9079
edisson89 <edisson.12@hotmail.com>
2023-03-30 10:17:41 -0500

    Agregando uun botom  Ramdom

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,29 +1,31 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-export default function SearchBar({onSearch}) {
-   const [id, setId] = useState('')
-   
-   function handleChange(event) {
-      event.preventDefault()
-      setId(event.target.value)
-     // console.log(event.target.value)
-   }
-    
-   function getRandom(max) {
-     max = 856 
-      return Math.floor(Math.random() * max);
-    }
-   return (
-      <div >
-         <input onChange={handleChange} name='search' type='search' value={id} />
-     <NavLink to='/home'>
+export default function SearchBar({ onSearch }) {
+  const [id, setId] = useState("");
 
-     <button onClick={()=> onSearch(id)}>Agregar</button>
-     </NavLink>
-     <NavLink to='/home'>
-      <button onClick={()=> onSearch(getRandom())}>Ramdom</button>
-     </NavLink>
-      </div>
-   );
+  function handleChange(event) {
+    event.preventDefault();
+    setId(event.target.value);
+  }
+
+  function getRandom() {
+   let max = 856;
+    return Math.floor(Math.random(0,max) * max );
+  }
+  return (
+
+   <div>
+      <input onChange={handleChange} name="search" type="search" value={id} />
+      <NavLink to="/home">
+        <button onClick={() => onSearch(id)}>Agregar</button>
+      </NavLink>
+       <NavLink to="/home">
+        <button onClick={() => onSearch(getRandom())}>Ramdom</button>
+      </NavLink>
+     
+    </div>
+ 
+    
+  )          
 }
