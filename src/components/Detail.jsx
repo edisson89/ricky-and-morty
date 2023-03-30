c2481161be47acd697a96caf714bb9d883a1f37b
edisson89 <edisson.12@hotmail.com>
2023-03-30 10:15:53 -0500

    Instalando axios

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,14 +1,14 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import axios from "axios";
+import  { NavLink, useParams } from "react-router-dom";
 import styles from "../App.module.css";
 
 const Detail = () => {
   const [character, setCharacter] = useState({});
+ 
   const { id } = useParams();
   useEffect(() => {
-    axios
-      .get(`https://rickandmortyapi.com/api/character/${id}`)
+    axios.get(`https://rickandmortyapi.com/api/character/${id}`)
       .then(({ data }) => {
         if (data.name) {
           setCharacter(data);
@@ -21,11 +21,15 @@ const Detail = () => {
 
   return (
     <div>
+      <NavLink to='/home'>
+        <button >Regresar Home</button>
+      </NavLink>
+      
       <h2>{character.name}</h2>
       <h2>{character.species}</h2>
       <h2>{character.status}</h2>
       <h2>{character.gender}</h2>
-      <h2>{character.origin?.name}</h2>
+      <h2>{character.origin && character.origin.name}</h2>
       <img className={styles.image} src={character.image} alt="imagen" />
     </div>
   );
