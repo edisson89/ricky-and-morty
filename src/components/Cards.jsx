d4da69fc86604e151e2abf751c30e0aaa9181518
edisson89 <edisson.12@hotmail.com>
2023-03-30 10:15:37 -0500

    Revisando props

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,15 @@
-import Card from "./Card";
+import React from 'react';
+import Card from './Card'
 import styles from "../App.module.css";
 
-export default function Cards(props) {
-  const { characters, onClose } = props;
+export default function Cards({ characters, onClose }) {
+ 
  
   return (
     <div className={styles.cards}>
       {characters &&
        characters.map(({ name, species, gender, image, id }) => (
+       
         <Card
           id={id}
           key={id}
@@ -19,5 +21,5 @@ export default function Cards(props) {
         />
       ))}
     </div>
-  );
+  )
 }
