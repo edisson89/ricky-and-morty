0aea89ac0de2b040977e066668d73b62d119210c
edisson89 <edisson.12@hotmail.com>
2023-03-30 10:14:48 -0500

    Actualizando funciones

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,20 @@
-import { Link } from 'react-router-dom';
-import styles from  '../App.module.css'
+import React from "react";
+import  { NavLink } from "react-router-dom";
+import styles from '../App.module.css'
 
-export default function Card(props) {
+export default function Card (props) {
    const {id,name, species,gender,image,onClose} = props
    return (
       <div className={styles.card}>
-         <Link to={`/detail/${id}`}>
-          <button id="cardButton" onClick={()=>onClose(id)}>X</button>
+          <button id="cardButton" onClick={() => onClose (id)}>X</button>
+         <NavLink to={`/detail/${id}`}>
          <h2>{name}</h2>
          <h2>{species}</h2>
          <h2>{gender}</h2>
          <img className={styles.image} src={image} alt="imagen"/> 
-         </Link>
+         </NavLink>
         
       </div>
-   );
+   )
 }
+  
