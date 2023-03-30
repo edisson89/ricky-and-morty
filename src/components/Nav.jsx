0d10cc9d14abcad37cf0d8f19370c99e39da2700
edisson89 <edisson.12@hotmail.com>
2023-03-30 10:17:20 -0500

    Agregando Routas

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import SearchBar from './SearchBar'
-import styles from '../App.module.css'
-import { NavLink} from 'react-router-dom'
+import React from "react";
+import SearchBar from "./SearchBar";
+import styles from "../App.module.css";
+import { NavLink } from "react-router-dom";
 
-
-const Nav = ({onSearch}) => {
+const Nav = ({ onSearch, logout }) => {
   return (
     <div className={styles.navbar}>
-      
-      <NavLink className={styles.home} to='/home'>
-      <button >Home</button>
+      <NavLink className={styles.home} to="/home">
+        <button>Home</button>
       </NavLink>
 
-      <NavLink className={styles.home} to='/about'>
-      <button >About</button>
+      <NavLink className={styles.home} to="/about">
+        <button>About</button>
       </NavLink>
-        <div className={styles.searchBar}>
-          <SearchBar onSearch={onSearch}/>
-        </div>
-        
+      <div className={styles.searchBar}>
+        <SearchBar onSearch={onSearch} />
+      </div>
+      <button onClick={logout}>Logout</button>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
