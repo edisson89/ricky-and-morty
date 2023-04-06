d3d349a97851ab9c2196923da6a782abc36c5551
edisson89 <edisson.12@hotmail.com>
2023-04-06 12:25:09 -0500

    Agregando  redduccers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,18 @@
 //createSstore
+// import { createStore } from 'redux'
+// import rootReducer from './reducer'
+
+// let store = createStore(rootReducer)
+
+// store.subscribe(() => console.log(store.getState()))
+import rootReducer from './reducer'
+
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({ 
+   reducer:{
+     reducer: rootReducer 
+   }
+   
+})
+
