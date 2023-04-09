4254711d98b289348165ac0da7f29c5eb67d07ca
edisson89 <edisson.12@hotmail.com>
2023-04-09 17:51:28 -0500

    cambios destructuring

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,13 @@ import styles from '../App.module.css'
 
 export default function Card (props) {
    const {id,name, species,gender,image,onClose} = props
+   
    return (
-      <div className={styles.card}>
-          <button id="cardButton" onClick={() => onClose (id)}>X</button>
+      <div  className={styles.card}>
+          <button name='button' onClick={() => onClose(id)}>X</button>
          <NavLink to={`/detail/${id}`}>
          <h2>{name}</h2>
+         <h2>{id}</h2>
          <h2>{species}</h2>
          <h2>{gender}</h2>
          <img className={styles.image} src={image} alt="imagen"/> 
