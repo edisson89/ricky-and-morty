ed8bd52b356ede96806c82c5d1ff793c237687a4
edisson89 <edisson.12@hotmail.com>
2023-04-28 19:01:07 -0500

    Actuualizando actions

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,14 +1,32 @@
-import { ADD_FAV, REMOVE_FAV } from "./action.types";
+import { ADD_FAV, REMOVE_FAV, ORDER, FILTER, RESET } from "./action.types";
 
 export function addFav(data) {
+  return {
+    type: ADD_FAV,
+    payload: data,
+  };
+}
+export function removeFav(id) {
+  return {
+    type: REMOVE_FAV,
+    payload: id,
+  };
+}
+export function order(order) {
+  return {
+    type: ORDER,
+    payload: order,
+  };
+}
+export function filter(gender) {
+  return {
+    type: FILTER,
+    payload: gender,
+  };
+}
+export function reset() {
     return {
-        type: ADD_FAV,
-        payload:data
-    }
-}
-export  function removeFav(id){
-    return{
-        type: REMOVE_FAV,
-        payload: id
-    }
-}
+      type: RESET,
+     
+    };
+  }
