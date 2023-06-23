5297fbf98d74829f2133d0a58229d9a028fb3de7
edisson89 <edisson.12@hotmail.com>
2023-06-23 10:12:36 -0500

    Cambios en las funciones

--- client/src/components/Characters.jsx
+++ client/src/components/Characters.jsx
@@ -1,33 +1,30 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import CardCharacter from "./CardCharacter";
 import { removeHome } from "../redux/action";
+import Card from "./Card"
 
 const Character = () => {
   const dispatch = useDispatch();
   const [results] = useSelector((state) => state.home);
-  
+
   function onClose(id) {
-    dispatch(removeHome(results.id));
+    dispatch(removeHome(id));
   }
-  
 
   return (
     <>
       {results &&
-        results.map(({id,name,species,gender,image}) => (
-         
-            <div key={id}>
-               
-              <CardCharacter
-                id={id}
-                name={name}
-                species={species}
-                gender={gender}
-                image={image}
-                onClose={()=>onClose(id)}
-              />
-            </div>
+        results
+          .map(({ id, name, species, gender, image }) => (
+            <Card
+              key={id}
+              id={id}
+              name={name}
+              species={species}
+              gender={gender}
+              image={image}
+              onClose={onClose}
+            />
           ))
           .reverse()}
     </>
