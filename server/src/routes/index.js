768dab61e6b988e03daf815bcec773c73a70adfd
edisson89 <edisson.12@hotmail.com>
2023-06-23 10:09:28 -0500

    Agregando ruta

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const router = express.Router();
 const { getCharById } = require("../controllers/getCharById");
 const { getCharDetails } = require("../controllers/getCharDetails");
 const {postFav, getFav,removeFav} = require("../controllers/postFav");
-const router = express.Router();
+const { getCharacterData } = require("../controllers/getCharacterData");
+
+router.get("/data",getCharacterData)
 
 router.get("/onsearch/:id", getCharById)
 router.get("/detail/:id",getCharDetails)
+
 
 router.post("/favs", postFav)
 router.get("/favs", getFav)
